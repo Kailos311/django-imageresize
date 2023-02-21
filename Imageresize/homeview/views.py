1eb881764d63c10d1ad5769e525edda7ff3595a3
AvramsDavis <115776806+AvramsDavis@users.noreply.github.com>
2023-02-21 21:37:05 +0900

    semi

--- Imageresize/homeview/views.py
+++ Imageresize/homeview/views.py
@@ -8,73 +8,55 @@ from PIL import Image
 import os
 from django.views import View
 from .form import *
+from django.conf import settings
 
 # Create your views here.
 # class HomeView(View):
 BaseURL = "http://localhost:8000"
 uploaded_image = ''
+url = ''
 def Home(request):
     form = StudentForm()
+    global uploaded_image
     uploaded_image = BaseURL + "/media/uploads/empty.png"
     return render(request, 'home.html', {'form' : form, 'imageurl' : uploaded_image})
-    # template = loader.get_template('home.html')
-    # return HttpResponse(template.render())
 
 @csrf_exempt
 def upload(request):
     if request.method == 'POST':
         form = StudentForm(request.POST, request.FILES)
-        uploaded_image = BaseURL + "/media/uploads/" + str(request.FILES['path'])
+        global uploaded_image
+        uploaded_image = "/uploads/" + str(request.FILES['path'])
+        global url
+        url = BaseURL + "/media/uploads/" + str(request.FILES['path'])
 
         if form.is_valid():
             form.save()
             # return redirect('success')
-            return render(request, 'home.html', {'form' : form, 'imageurl' : uploaded_image})
-    # print(request.POST['url'])
-    # # print(request.POST['file'])
-    # # saveimage(request.POST['url'])
-    # template = loader.get_template('home.html')
-    # return HttpResponse(template.render())
+            return render(request, 'home.html', {'form' : form, 'imageurl' : url})
 
+@csrf_exempt
 def Save(request):
+    file_path = os.path.join(settings.MEDIA_ROOT,'uploads')
+    file = request.POST['name'].split(".")
+    resize_path = os.path.join(settings.MEDIA_ROOT, 'resize')
+    save_path = os.path.join(resize_path, file[0])
+    os.mkdir(save_path)
+    thumbnail_path = os.path.join(save_path, 'thumbnail.' + file[1])
+    origin_path = os.path.join(save_path, 'origin.' + file[1])
+    middle_path = os.path.join(save_path, 'middle.' + file[1])
+    big_path = os.path.join(save_path, 'big.' + file[1])
+    
     form = StudentForm()
-    if request.POST['type'] == 'Custom':
-        saveimage(type="Custom", w=request.POST['width'], h=request.POST['height'])
-    else :
-        saveimage(type=request.POST['type'])
-    return render(request, 'home.html', {'form' : form, 'imageurl' : uploaded_image})
+    file_origin = os.path.join(file_path, request.POST['name'])
+    image = Image.open(file_origin)
+    origin = image.save(origin_path)
+    new_thumbnail = image.resize((90,90))
+    new_middle = image.resize((150,150))
+    new_big = image.resize((1000,600))
 
-def saveimage(self, **kwargs):
-    if kwargs['type'] == "Custom":
-        image = Image.open(uploaded_image)
-        new_image = image.resize((kwargs['w'], kwargs['h']))
-        new_image.save(self.BaseURL + '/resize/myimage_500.jpg')
-    # absolute_path = os.path.dirname(os.path.abspath(__file__))
-    # filepath = absolute_path + "/templates/example.jpg"
-    # image = Image.open(filepath)
-    # new_image = image.resize((500, 500))
-    # new_image.save(absolute_path + '/myimage_500.jpg')
-
-
-# from django.http import HttpResponse
-# from django.shortcuts import render, redirect
-# from .form import *
-
-
-# def avatarView(request):
-
-#     if request.method == 'POST':
-#         print(request.POST)
-#         print(request.FILES)
-#         form = StudentForm(request.POST, request.FILES)
-
-#         if form.is_valid():
-#             form.save()
-#             return redirect('success')
-#     else:
-#         form = StudentForm()
-#     return render(request, 'studentform.html', {'form' : form})
-
-
-# def uploadok(request):
-#     return HttpResponse(' upload successful')
+    new_thumbnail.save(thumbnail_path)
+    new_middle.save(middle_path)
+    new_big.save(big_path)
+    
+    return render(request, 'home.html', {'form' : form, 'imageurl' : url})
