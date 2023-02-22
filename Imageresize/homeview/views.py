41dd0a09bd2fe29c1d6938885db6e6af40137a0a
AvramsDavis <115776806+AvramsDavis@users.noreply.github.com>
2023-02-22 09:37:59 +0900

    final

--- Imageresize/homeview/views.py
+++ Imageresize/homeview/views.py
@@ -41,22 +41,71 @@ def Save(request):
     file = request.POST['name'].split(".")
     resize_path = os.path.join(settings.MEDIA_ROOT, 'resize')
     save_path = os.path.join(resize_path, file[0])
-    os.mkdir(save_path)
-    thumbnail_path = os.path.join(save_path, 'thumbnail.' + file[1])
-    origin_path = os.path.join(save_path, 'origin.' + file[1])
-    middle_path = os.path.join(save_path, 'middle.' + file[1])
-    big_path = os.path.join(save_path, 'big.' + file[1])
+    try:
+        os.mkdir(save_path)
+    except:
+        pass
     
     form = StudentForm()
     file_origin = os.path.join(file_path, request.POST['name'])
     image = Image.open(file_origin)
-    origin = image.save(origin_path)
-    new_thumbnail = image.resize((90,90))
-    new_middle = image.resize((150,150))
-    new_big = image.resize((1000,600))
-
-    new_thumbnail.save(thumbnail_path)
-    new_middle.save(middle_path)
-    new_big.save(big_path)
+    
+    webp_path = os.path.join(save_path, 'webp')
+    
+    try:
+        os.mkdir(webp_path)
+        webp_thumbnail_path = os.path.join(webp_path, 'thumbnail.webp')
+        webp_origin_path = os.path.join(webp_path, 'origin.webp')
+        webp_middle_path = os.path.join(webp_path, 'middle.webp')
+        webp_big_path = os.path.join(webp_path, 'big.webp')
+        
+        webp_origin = image.save(webp_origin_path)
+        webp_new_thumbnail = image.resize((90,90))
+        webp_new_middle = image.resize((150,150))
+        webp_new_big = image.resize((1000,600))
+        webp_new_thumbnail.save(webp_thumbnail_path)
+        webp_new_middle.save(webp_middle_path)
+        webp_new_big.save(webp_big_path)
+    except:
+        pass
+    
+    
+    if request.POST['type'] == "logo":
+        png_path = os.path.join(save_path, 'png')
+        try:
+            os.mkdir(png_path)
+            png_thumbnail_path = os.path.join(png_path, 'thumbnail.png')
+            png_origin_path = os.path.join(png_path, 'origin.png')
+            png_middle_path = os.path.join(png_path, 'middle.png')
+            png_big_path = os.path.join(png_path, 'big.png')
+            png_new_thumbnail = image.resize((90,90))
+            png_new_middle = image.resize((150,150))
+            png_new_big = image.resize((1000,600))
+            
+            png_origin = image.save(png_origin_path)
+            png_new_thumbnail.save(png_thumbnail_path)
+            png_new_middle.save(png_middle_path)
+            png_new_big.save(png_big_path)
+        except:
+            pass
+    else :
+        jpg_path = os.path.join(save_path, 'jpg')
+        try:
+            os.mkdir(jpg_path)
+            jpg_thumbnail_path = os.path.join(jpg_path, 'thumbnail.jpg')
+            jpg_origin_path = os.path.join(jpg_path, 'origin.jpg')
+            jpg_middle_path = os.path.join(jpg_path, 'middle.jpg')
+            jpg_big_path = os.path.join(jpg_path, 'big.jpg')
+            jpg_new_thumbnail = image.resize((90,90))
+            jpg_new_middle = image.resize((150,150))
+            jpg_new_big = image.resize((1000,600))
+            
+            jpg_origin = image.save(jpg_origin_path)
+            jpg_new_thumbnail.save(jpg_thumbnail_path)
+            jpg_new_middle.save(jpg_middle_path)
+            jpg_new_big.save(jpg_big_path)
+        except:
+            pass
+        
     
     return render(request, 'home.html', {'form' : form, 'imageurl' : url})
